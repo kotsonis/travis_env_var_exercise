743bc60995916873e662cd98ba615e3e25712933
kotsonis <stefankotsonis@gmail.com>
2021-03-14 12:52:12 +0100

    feat: implement Travis environment variables

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
+  let my_name = process.env.SECRET_NAME;
   while(true) {
-    console.log("Microservices rock!");
+    console.log(`Hey ${my_name} Microservices rock!`);
     await sleep(5000);
   }
 }
